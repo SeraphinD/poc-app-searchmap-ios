1d6a1ab25b17c1010d40e0f1702bd13d162ee8af
Séraphin Desumeur <seraphin.desumeur@ineat-conseil.fr>
2018-04-01 18:28:04 +0200

    add mapbox map and center button

---
+++ MapSearch/BlurredUIView.swift
@@ -0,0 +1,35 @@
+//
+//  BlurredUIView.swift
+//  mapsearch
+//
+//  Created by Seraphin DESUMEUR on 01/04/2018.
+//  Copyright © 2018 Seraphin DESUMEUR. All rights reserved.
+//
+
+import UIKit
+
+final class BlurredUIView: UIView {
+    
+    @IBInspectable var blurBackground: Bool = false {
+        didSet {
+            removeBlurEffect()
+            if blurBackground {
+                addBlurEffect()
+            }
+        }
+    }
+    
+    private func addBlurEffect() {
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = bounds
+        clipsToBounds = true
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        insertSubview(blurEffectView, at: 0)
+    }
+    
+    private func removeBlurEffect() {
+        let blurredEffectViews = subviews.filter {$0 is UIVisualEffectView}
+        blurredEffectViews.forEach { $0.removeFromSuperview() }
+    }
+}
---
+++ MapSearch/UIView+Utils.swift
@@ -0,0 +1,82 @@
+//
+//  UIView+Utils.swift
+//  mapsearch
+//
+//  Created by Seraphin DESUMEUR on 01/04/2018.
+//  Copyright © 2018 Seraphin DESUMEUR. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        set {
+            layer.shadowColor = newValue!.cgColor
+        }
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        set {
+            layer.shadowOpacity = newValue
+        }
+        get {
+            return layer.shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGPoint {
+        set {
+            layer.shadowOffset = CGSize(width: newValue.x, height: newValue.y)
+        }
+        get {
+            return CGPoint(x: layer.shadowOffset.width, y:layer.shadowOffset.height)
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        set {
+            layer.masksToBounds = false
+            layer.shadowRadius = newValue
+        }
+        get {
+            return layer.shadowRadius
+        }
+    }
+}
